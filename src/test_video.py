fc90014bf58081a5b1ce063c9eda06a5fb61d78b
Khang Nguyen <khang@Khangs-MacBook-Pro-133.local>
2025-09-09 23:10:33 -0700

    Tweaked features in pytesseract to allow for more accurate rea dings of the elixir bar

--- src/test_video.py
+++ src/test_video.py
@@ -2,24 +2,29 @@ import cv2
 from matplotlib.pylab import svd
 from roboflow import Roboflow
 from ultralytics import YOLO
-import random
-import supervision as sv
-import os
-import time as timer
 from dotenv import load_dotenv
 import pytesseract
+import numpy as np
 
 def read_image(img):
-     #img = cv2.imread(file_path)
+     config = "--psm 13 -c tessedit_char_whitelist=0123456789"
+     #img = Image.open(img)
      img = preprocess(img)
-     text = pytesseract.image_to_string(img)
-     return text
+     text = pytesseract.image_to_string(img, config = config)
+     cv2.imshow("video", img)
+     print(text.strip())
+     return text.strip()
 
 def preprocess(img):
-    #img =  cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-   # img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY +cv2.THRESH_OTSU)[1]
-    #img = cv2.medianBlur(img, 5)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.medianBlur(img, 3)
+    img = cv2.resize(img, None, fx = 5, fy = 5, interpolation=cv2.INTER_CUBIC)
+ 
+
+
+
     return(img)
+
 
 
 
@@ -29,19 +34,22 @@ path = "data/raw/test_video_1.MP4"
 output_path = "data/labels/annotated_video.mp4"
 
 
+
+
+
 def frame_reader(path, output_path, model):
     #grab the video based on file path
-   
+    index = 0
 
     cap = cv2.VideoCapture(path)
-    frames = []
-    time = []
+
     fps = cap.get(cv2.CAP_PROP_FPS)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(output_path, fourcc, fps, (width,height))
-    frame_skipper = 15 #display frame once every 15 frames
+
+    already_mapped = False
 
     for result in model.predict(
         path, 
@@ -55,21 +63,27 @@ def frame_reader(path, output_path, model):
         verbose = False,
 
     ): 
-        print(f'we have this many detections: {len(result)}')
+        #print(f'we have this many detections: {len(result)}')
+        
         for detection in result:
             box = detection.boxes
             class_index = int(box.cls)
             class_name = result.names[class_index]
 
-            if class_name == "elixir bar":
-                 x1, y1, x2, y2 = map(int, (box.xyxy[0].tolist()))
+            if class_name == "elixir bar" and index % 5 ==0:
+                 if already_mapped == False:   
+                    x1, y1, x2, y2 = map(int, (box.xyxy[0].tolist()))
+                    print("mapping again!")
+                    already_mapped = True
                  img = result.orig_img
-                 elixir_region = result.orig_img[y1:y2, x1:x2]
-                 print(read_image(elixir_region))
+                 elixir_region = img[y1+4:y2-30, x1+65:x2-80]
+                 print(f'Elixir count: {read_image(elixir_region)}')
+
 
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+        index += 1
         pass
     
 
@@ -92,6 +106,7 @@ model_path = "/opt/homebrew/runs/detect/train3/weights/best.pt"
 model = YOLO(model_path)
 
 frame_reader(path, output_path, model)
+
 
 
 
